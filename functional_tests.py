bd06dc753eb60b7ee35e3c3165aa8404685d850f
Spalns <sachtmann121@gmail.com>
2016-12-20 23:25:41 -0800

    Added helper method to functional test

--- functional_tests.py
+++ functional_tests.py
@@ -61,12 +61,8 @@ class NewVisitorTest(unittest.TestCase):
 
 		# The page updates again, and now shows both items on her list
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly',
-			[row.text for row in rows]
-		)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
@@ -77,6 +73,11 @@ class NewVisitorTest(unittest.TestCase):
 		# She visits that URL - her to-do list is still there.
 
 		# Satisfied, she goes back to sleep
+
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')

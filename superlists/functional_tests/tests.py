124e607c7964b3e95859862ff98aa33a862dc04e
Spalns <sachtmann121@gmail.com>
2016-12-26 20:22:33 -0800

    ne wurl, view and template to display lists, also almost all functionality working

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -24,7 +24,7 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def test_can_start_a_list_and_retrive_it_later(self):
+	def test_can_start_a_list_for_one_user(self):
 		#Edith has heard about a cool new online to-do app. She goes
 		#to check out it's homepage...
 		self.browser.get(self.live_server_url)
@@ -51,8 +51,9 @@ class NewVisitorTest(LiveServerTestCase):
 		#in the todo lists
 
 		inputbox.send_keys(Keys.ENTER)
-		edith_list_url = self.browser.current_url
-		self.assertRegex(edith_list_url, 'lists.+')
+		#edith_list_url = self.browser.current_url
+		#self.assertRegex(edith_list_url, '/lists/.+')
+		time.sleep(1)
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		time.sleep(1) #use this to avoid StaleElementReferenceException
@@ -71,7 +72,24 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-		# Now a new user, francis comes along tot he site.
+		#satisfied, she goes back to sleep
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+
+		# Edith starts a new todo list
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+
+		time.sleep(1)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+		# She notices her list has a unique url
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
+
+		# Now a new user, francis comes along to the site.
 
 		## We use a new browser session to make sure no information of
 		## Edith's is coming through from cookies etc
@@ -92,21 +110,22 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 
+		time.sleep(1)
 		# Francis gets his own unique URL
-		fracis_list_url = self.browser.current_url
-		self.assertRegex(fracis_list_url, 'lists/.+')
-		self.assertNotEqual(fracis_list_url, edith_list_url)
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
 
 		#Again, there is no trace of Edith's list
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 
-		self.fail('Finish the test!')
+		#self.fail('Finish the test!')
 
 		# She visits that URL - her to-do list is still there.
 
-		# Satisfied, she goes back to sleep
+		# Satisfied, They both go back to sleep
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')

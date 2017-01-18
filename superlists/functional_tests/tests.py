431970416ba76ca9a133921f300b6caf1d296637
spa <sachtmann121@gmail.com>
2017-01-18 01:52:08 -0800

    Fixed the live server url bit

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -6,10 +6,21 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 #import unittest
 import time
+import sys
 #from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
 
 	def setUp(self):
 		binary = FirefoxBinary(r'/home/spa/firefox/firefox') 
@@ -24,7 +35,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_for_one_user(self):
 		#Edith has heard about a cool new online to-do app. She goes
 		#to check out it's homepage...
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		#she notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -74,7 +85,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 
 		# Edith starts a new todo list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
@@ -96,7 +107,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 		# Francis visits the home page. There is no sign of Edith's
 		# list.
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -127,7 +138,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		#She notices the input box is nicely centered
